d6e308815efd6b47c33164a8d0ddba21268cbd68
Derpius <49565664+Derpius@users.noreply.github.com>
2025-05-04 00:03:55 +0100

    Improve logging

--- src/main.ts
+++ src/main.ts
@@ -9,32 +9,43 @@ const action = core.getInput("action");
 const vcpkgArchivePath = "/github/workspace/.cache/vcpkg/archives";
 
 if (action == "restore") {
-  core.info("Restoring vcpkg cache");
-  const octokit = github.getOctokit(token!);
+  await core.group("Restoring vcpkg cache", async () => {
+    const octokit = github.getOctokit(token!);
 
-  const { data: { actions_caches: actionsCaches } } = await octokit.rest.actions.getActionsCacheList(
-    {
-      owner: "TAServers",
-      repo: "TASBox",
-      key: CACHE_KEY_PREFIX,
-      per_page: 100, // TODO: Handle pagination
-    },
-  );
+    const { data: { actions_caches: actionsCaches } } = await octokit.rest.actions.getActionsCacheList(
+      {
+        owner: "TAServers",
+        repo: "TASBox",
+        key: CACHE_KEY_PREFIX,
+        per_page: 100, // TODO: Handle pagination
+      },
+    );
 
-  for (const cacheEntry of actionsCaches) {
-    await cache.restoreCache([getCacheRestorePath(vcpkgArchivePath, cacheEntry.key!)], cacheEntry.key!);
-  }
+    for (const cacheEntry of actionsCaches) {
+      const cacheKey = cacheEntry.key!;
+      const archivePath = getCacheRestorePath(vcpkgArchivePath, cacheEntry.key!);
+      core.info(`Restoring '${cacheKey}' to '${archivePath}'`);
+
+      await cache.restoreCache([archivePath], cacheEntry.key!);
+    }
+  });
 } else if (action == "save") {
-  for await (const directory of Deno.readDir(vcpkgArchivePath)) {
-    if (!directory.isDirectory) {
-      core.warning(`Ignoring file '${directory.name}' in top level of vcpkg archive`);
-      continue;
-    }
+  await core.group("Saving vcpkg cache", async () => {
+    for await (const directory of Deno.readDir(vcpkgArchivePath)) {
+      if (!directory.isDirectory) {
+        core.warning(`Ignoring file '${directory.name}' in top level of vcpkg archive`);
+        continue;
+      }
 
-    for await (const file of Deno.readDir(path.join(vcpkgArchivePath, directory.name))) {
-      await cache.saveCache([path.join(vcpkgArchivePath, directory.name, file.name)], getCacheKey(file.name));
+      for await (const file of Deno.readDir(path.join(vcpkgArchivePath, directory.name))) {
+        const cacheKey = getCacheKey(file.name);
+        const archivePath = path.join(vcpkgArchivePath, directory.name, file.name);
+        core.info(`Saving '${archivePath}' to '${cacheKey}'`);
+
+        await cache.saveCache([], getCacheKey(file.name));
+      }
     }
-  }
+  });
 } else {
-  core.setFailed(`Invalid action ${action}`);
+  core.setFailed(`Invalid action '${action}'`);
 }
