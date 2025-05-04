a1e5564d9c07943b3f8643c677930c03a6df71d1
Derpius <49565664+Derpius@users.noreply.github.com>
2025-05-04 12:31:27 +0100

    Catch file read errors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import cache from "@actions/cache";
 import core from "@actions/core";
 import * as path from "@std/path";
-import {CACHE_KEY_PREFIX, getCacheKey, getCacheRestorePath, getExistingCacheEntries} from "./helpers.ts";
+import { CACHE_KEY_PREFIX, getCacheKey, getCacheRestorePath, getExistingCacheEntries } from "./helpers.ts";
+import { FilesNotFoundError } from "npm:@actions/cache@4.0.3/lib/internal/shared/errors.d.ts";
 
 const token = core.getInput("token");
 const action = core.getInput("action");
@@ -12,8 +13,8 @@ if (action == "restore") {
     const actionsCaches = await getExistingCacheEntries(token!);
 
     if (actionsCaches.length < 1) {
-        core.info(`No cache entries found with prefix '${CACHE_KEY_PREFIX}'`);
-        return;
+      core.info(`No cache entries found with prefix '${CACHE_KEY_PREFIX}'`);
+      return;
     }
 
     await Promise.all(actionsCaches.map(async (cacheKey) => {
@@ -27,24 +28,30 @@ if (action == "restore") {
   await core.group("Saving vcpkg cache", async () => {
     const actionsCaches = new Set(await getExistingCacheEntries(token!));
 
-    for await (const directory of Deno.readDir(vcpkgArchivePath)) {
-      if (!directory.isDirectory) {
-        core.warning(`Ignoring file '${directory.name}' in top level of vcpkg archive`);
-        continue;
-      }
-
-      for await (const file of Deno.readDir(path.join(vcpkgArchivePath, directory.name))) {
-        const cacheKey = getCacheKey(file.name);
-        const archivePath = path.join(vcpkgArchivePath, directory.name, file.name);
-
-        if (actionsCaches.has(cacheKey)) {
-          core.info(`Skipping '${archivePath}' as already present in cache`);
+    try {
+      for await (const directory of Deno.readDir(vcpkgArchivePath)) {
+        if (!directory.isDirectory) {
+          core.warning(`Ignoring file '${directory.name}' in top level of vcpkg archive`);
+          continue;
         }
 
-        core.info(`Saving '${archivePath}' to '${cacheKey}'`);
+        for await (const file of Deno.readDir(path.join(vcpkgArchivePath, directory.name))) {
+          const cacheKey = getCacheKey(file.name);
+          const archivePath = path.join(vcpkgArchivePath, directory.name, file.name);
 
-        await cache.saveCache([archivePath], getCacheKey(file.name));
+          if (actionsCaches.has(cacheKey)) {
+            core.info(`Skipping '${archivePath}' as already present in cache`);
+          }
+
+          core.info(`Saving '${archivePath}' to '${cacheKey}'`);
+
+          await cache.saveCache([archivePath], getCacheKey(file.name));
+        }
       }
+    } catch (error) {
+      const message = error instanceof Error ? error.message : String(error);
+
+      core.error(message);
     }
   });
 } else {
